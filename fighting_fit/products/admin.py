b0210e9a1d2ae744f3d27cf3b56aa16ee193b66e
SCott <scott.leighton@hotmail.co.uk>
2024-02-24 19:55:59 +0000

    Edit:Customized admin

--- fighting_fit/products/admin.py
+++ fighting_fit/products/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_sku',
+        'product_name',
+        'product_category',
+        'product_price',
+        'product_image',
+    )
+
+    ordering = ('product_sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
