28c98933493c0739da55a513795d57daa3c0dafd
SCott <scott.leighton@hotmail.co.uk>
2024-02-26 11:23:11 +0000

    Feat:Add category filtering for products and update main navigation links

--- fighting_fit/products/views.py
+++ fighting_fit/products/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 
 def all_products(request):
     products = Product.objects.all()
-
+    categories = None
     query = None
+    
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(product_category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
         if 'q' in request.GET:
@@ -23,6 +29,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
