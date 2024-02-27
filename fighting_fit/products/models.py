16af1ef59355b85cf82e3dfce8828a918f45603c
SCott <scott.leighton@hotmail.co.uk>
2024-02-27 12:39:32 +0000

    Feat:Add product size selection to bag.html and product_detail.html

--- fighting_fit/products/models.py
+++ fighting_fit/products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     product_sku = models.CharField(max_length=254, null=True, blank=True)
     product_name = models.CharField(max_length=254)
     product_description = models.TextField()
+    product_has_sizes = models.BooleanField(default=False, null=True, blank=True)
     product_price = models.DecimalField(max_digits=6, decimal_places=2)
     product_image_url = models.URLField(max_length=1024, null=True, blank=True)
     product_image = models.ImageField(null=True, blank=True)
