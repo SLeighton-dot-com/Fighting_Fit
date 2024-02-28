80b21f3abb21f4f6a76fee325a8602109bdcb121
SCott <scott.leighton@hotmail.co.uk>
2024-02-28 12:52:11 +0000

    Feat:Added toast messages

--- fighting_fit/bag/views.py
+++ fighting_fit/bag/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
+
+from products.models import Product
 
 def view_bag(request):
     """ A view to return the shopping bag content page"""
@@ -11,6 +11,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     try:
+        product = Product.objects.get(pk=item_id)
         quantity = int(request.POST.get('quantity'))
         redirect_url = request.POST.get('redirect_url')
         size = None
@@ -31,13 +32,20 @@ def add_to_bag(request, item_id):
                 bag[item_id] += quantity
             else:
                 bag[item_id] = quantity
-                messages.success(request, 'Added to your bag')
+                messages.success(request, f'Added {product.product_name} to your bag')
                 
         request.session['bag'] = bag
-        return HttpResponse(status=200)
+        return redirect(redirect_url)
 
+    except Product.DoesNotExist:
+        messages.error(request, "Product not found!")
+        return redirect('some_default_route')
+    except ValueError:
+        messages.error(request, "Invalid quantity!")
+        return redirect(redirect_url)
     except Exception as e:
-        return HttpResponse(status=500)
+        messages.error(request, f"Error adding product to bag: {str(e)}")
+        return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
