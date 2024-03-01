a85486a8f121becd1e35e28d3e12d93dbe533b66
SCott <scott.leighton@hotmail.co.uk>
2024-03-01 18:33:22 +0000

    Feat:Add confirmation email templates and send confirmation email in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem, Product
 from profiles.models import UserProfile
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 import json
 import time
+import stripe
 
 
 class StripeWH_Handler:
@@ -11,6 +15,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+        
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )    
 
     def handle_event(self, event):
         """
@@ -25,21 +46,9 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        pid = intent.id
-        bag = intent.metadata.bag
-        save_info = intent.metadata.save_info
-        
         stripe_charge = stripe.Charge.retrieve(
             intent.latest_charge
-)
-
-        billing_details = stripe_charge.billing_details
-        shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)
-
-        for field, value in shipping_details.address.items():
-            if value == "":
-                shipping_details.address[field] = None
+        )
                 
         profile = None
         username = intent.metadata.username
@@ -79,6 +88,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -123,6 +133,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+                self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
