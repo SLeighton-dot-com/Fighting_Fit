0f6020175111da3d2eb6e9179070ef088f5b3669
SCott <scott.leighton@hotmail.co.uk>
2024-03-17 14:36:44 +0000

    Edit:Added app name to template tags for profiles app

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('subscribe/', views.newsletter_subscribe, name='newsletter_subscribe'),
 ]
