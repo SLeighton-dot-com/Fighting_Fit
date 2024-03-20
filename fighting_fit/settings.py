51dec24652e4aab91b4731f7579292b785b2c7b1
SCott <scott.leighton@hotmail.co.uk>
2024-03-20 12:00:38 +0000

    Edit:Add Whitenoise middleware to settings.py and requirements.txt

--- fighting_fit/settings.py
+++ fighting_fit/settings.py
@@ -63,7 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fighting_fit.urls'
