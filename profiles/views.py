abdc2022a8dd1b4db6c5089ef505db09e79383a5
SCott <scott.leighton@hotmail.co.uk>
2024-03-24 16:07:49 +0000

    Edit:Update order history permission check

--- profiles/views.py
+++ profiles/views.py
@@ -37,7 +37,7 @@ def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
     
     # Check if the logged-in user is the one who made the order
-    if order.user != request.user:
+    if order.email != request.user.email:
         # If not, show error message and redirect to profile page
         messages.error(request, "You don't have permission to view this order.")
         redirect('Profile')
